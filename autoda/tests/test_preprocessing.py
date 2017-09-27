ac0df98e9a71c5aff9ae42ad3d29b93193c082c5
Misgana Negassi Goitom <misganazemo@gmail.com>
2017-09-27 16:15:36 +0200

    dummy test for preprocessing script

---
+++ autoda/tests/test_preprocessing.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: iso-8859-15 -*-
+
+import unittest
+
+class TestSimpleBatchGeneration(unittest.TestCase):
+
+    """Test simple case for batch generation with valid inputs"""
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        unittest.TestCase.__init__(self)
+
+
+class TestCornerCases(unittest.TestCase):
+
+    """Docstring for TestCornerCases. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        unittest.TestCase.__init__(self)
+
+
+class TestInvalidInputs(unittest.TestCase):
+
+    """Docstring for TestInvalidInputs. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        unittest.test.__init__(self)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
