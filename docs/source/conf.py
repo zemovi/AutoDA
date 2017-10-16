f8805ed5f2623eabc8446a2bcfb85a981d8eb905
Misgana Negassi Goitom <misganazemo@gmail.com>
2017-10-16 18:00:26 +0200

    interim

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,19 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sys
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+
+MOCK_MODULES = ['smac', 'tensorflow', ]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 
 # -- General configuration ------------------------------------------------
