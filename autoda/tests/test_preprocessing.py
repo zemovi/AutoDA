15193dcda438398dbd1cc48e977702d8cf64c355
Misgana Negassi Goitom <misganazemo@gmail.com>
2017-10-25 14:48:45 +0200

    some tests for batch generation

--- autoda/tests/test_preprocessing.py
+++ autoda/tests/test_preprocessing.py
@@ -2,34 +2,58 @@
 # -*- coding: iso-8859-15 -*-
 
 import unittest
+import pytest
+
+try:
+    from hypothesis import given
+    from hypothesis.strategies import integers
+except ImportError:
+    HYPOTHESIS_INSTALLED = False
+else:
+    HYPOTHESIS_INSTALLED = True
+
+from keras.datasets import cifar10
+from autoda.preprocessing import generate_batches
+
+(CIFAR10_X_TRAIN, CIFAR10_Y_TRAIN), *_ = cifar10.load_data()
+CIFAR10_N_DATAPOINTS, *_ = CIFAR10_X_TRAIN.shape
+
 
 class TestSimpleBatchGeneration(unittest.TestCase):
-
     """Test simple case for batch generation with valid inputs"""
+    @pytest.mark.skipif(not HYPOTHESIS_INSTALLED, reason="Hypothesis not installed!")
+    @given(integers(min_value=1, max_value=CIFAR10_N_DATAPOINTS))
+    def test_batchsize_equals_n_images(self, batch_size):
+        """TODO: Docstring for test_batchsize_equals_n_images.
+        Returns
+        -------
+        TODO
 
-    def __init__(self):
-        """TODO: to be defined1. """
-        unittest.TestCase.__init__(self)
+        """
 
+        generator = generate_batches(
+            CIFAR10_X_TRAIN, CIFAR10_Y_TRAIN, batch_size=batch_size
+        )
 
-class TestCornerCases(unittest.TestCase):
+        x_batch, y_batch = next(generator)
+        n_images, *_ = x_batch.shape
 
-    """Docstring for TestCornerCases. """
+        assert n_images == batch_size
 
-    def __init__(self):
-        """TODO: to be defined1. """
-        unittest.TestCase.__init__(self)
+    @pytest.mark.skipif(not HYPOTHESIS_INSTALLED, reason="Hypothesis not installed!")
+    @given(integers(max_value=0))
+    def test_invalid_batchsize_error(self, batch_size):
+        with pytest.raises(AssertionError):
+            generator = generate_batches(
+                CIFAR10_X_TRAIN, CIFAR10_Y_TRAIN, batch_size=batch_size
+            )
+            next(generator)
 
+    # XXX: Test batchsize larger n_images
 
-class TestInvalidInputs(unittest.TestCase):
+    # XXX: Test that all images in batches are in dataset
 
-    """Docstring for TestInvalidInputs. """
-
-    def __init__(self):
-        """TODO: to be defined1. """
-        unittest.test.__init__(self)
-
-
+    # XXX: Test that batches are subsequent and have no overlap
 
 
 if __name__ == "__main__":
