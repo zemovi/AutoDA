8d81af4de33dd873f6d704165b27aabf2ca204d2
Misgana Negassi Goitom <misganazemo@gmail.com>
2018-01-05 14:40:46 +0100

    hyperband experiment

--- experiments/run_hyperband.py
+++ experiments/run_hyperband.py
@@ -2,13 +2,15 @@ import hpbandster
 import hpbandster.distributed.utils
 from hpbandster.distributed.worker import Worker
 
+import pickle
+import json
 import sys
+import os
 from os.path import abspath, join as path_join
 sys.path.insert(0, abspath(path_join(__file__, "..", "..")))
 
 from autoda.data_augmentation import ImageAugmentation
 from autoda.networks.train import objective_function
-#from test_cifar10 import objective_function
 from keras.datasets import cifar10
 
 import logging
@@ -33,19 +35,16 @@ class ImageAugmentationWorker(Worker):
             For dramatization, the function sleeps for one second, which emphasizes
             the speed ups achievable with parallel workers.
             """
-            print("Budget", budget)
 
             results = self.function(
                 benchmark="AlexNet", configuration=config, dataset=self.dataset, max_epochs=40, batch_size=512, time_budget=budget
             )
 
-            # results = self.function(config)
-            print("RES", results)
-            loss = results["validation_loss"]
+            validation_error = results["validation_error"]
             # loss = results
 
             return({
-                'loss': loss,   # this is the a mandatory field to run hyperband
+                'loss': validation_error,   # this is the a mandatory field to run hyperband
                 'info': results  # can be used for any user-defined information - also mandatory
             })
 
@@ -75,16 +74,29 @@ HB = hpbandster.HB_master.HpBandSter(
     config_generator=CG,
     run_id='0',
     eta=2,
-    min_budget=200,
-    max_budget=900,      # HB parameters
+    min_budget=50,
+    max_budget=1800,      # HB parameters
     nameserver=nameserver,
     ns_port=ns_port,
     job_queue_sizes=(0, 1)
 )
-# runs one iteration if at least one worker is available
-res = HB.run(1, min_n_workers=1)
-
+# runs one iteration if at least one worker is available, first parameter
+# is number of successive halving
+res = HB.run(5, min_n_workers=1)
 # shutdown the worker and the dispatcher
 HB.shutdown(shutdown_workers=True)
 
+
+path = path_join(abspath("."), "AutoData/hyperband")
+
+with open(os.path.join(path, 'hyperband.pickle'), 'wb') as f:
+    # Pickle the 'res' object from hyperband to enable us to extract useful information
+    pickle.dump(res, f)
+
+
 print(res.get_incumbent_trajectory())
+best_config = res.get_incumbent_trajectory()
+
+
+with open(os.path.join(path, "hyperband_result.json"), "w") as fh:
+        json.dump(best_config, fh)
